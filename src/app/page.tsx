6cb93793d9b0b1e461891b920ec27d82ce4cdbd1
AMMAR AHMED <ammarahmed2037@gmail.com>
2025-09-05 10:21:38 +0000

    Try fixing this error: `Runtime Error: Error: Failed to execute 'animate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ const fadeIn = (direction = 'up', delay = 0) => ({
     transition: {
       delay,
       duration: 0.8,
-      ease: [0.6, 0.05, -0.01, 0.9]
+      ease: [0.6, 0.05, 0.01, 0.9]
     }
   },
 });
