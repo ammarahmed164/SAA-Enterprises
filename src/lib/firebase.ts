a28b9f79946834e67a965723c0087ff04a8062e1
AMMAR AHMED <ammarahmed2037@gmail.com>
2025-09-06 08:25:16 +0000

    Firebase: Error (auth/api-key-not-valid.-please-pass-a-valid-api-key.).

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -17,7 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
